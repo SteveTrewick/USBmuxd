2356e4554bed95c74fc2f1a537d063be07d28a9e
Steve Trewick <steve.trewick@icloud.com>
2025-01-25 18:25:00 +0000

    add tag param

--- Sources/USBmuxdCore/MessageBuilder.swift
+++ Sources/USBmuxdCore/MessageBuilder.swift
@@ -6,14 +6,14 @@ public struct MessageBuilder {
   
   public init() {}
   
-  public func build(from dict: [String : Any] ) -> Data? {
+  public func build(from dict: [String : Any], tag: UInt32 ) -> Data? {
     // first create the PList so we know how long it is
     guard
       let pldata = try? PropertyListSerialization.data(fromPropertyList: dict, format: .xml, options: 0)
     else {
       return nil
     }
-    var header = USBMuxdHeader(length: UInt32(16 + pldata.count), version: 1, type: 8, tag: 0xdeadbeef)
+    var header = USBMuxdHeader(length: UInt32(16 + pldata.count), version: 1, type: 8, tag: tag)
     let hdata  = Data(bytes: &header, count: 16)
     
     return hdata + pldata
