b4f4263c35ada6b9032d561f010529cd696a93f6
Steve Trewick <steve.trewick@icloud.com>
2025-06-25 18:30:36 +0100

    update package version

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     name        : "USBmuxd",
     products    : [ .library( name: "USBmuxd", targets: ["USBmuxd"] ), ],
     dependencies: [
-      .package(name: "GCDSocket", url: "https://github.com/SteveTrewick/GCDSocket", from: "1.0.3")
+      .package(name: "GCDSocket", url: "https://github.com/SteveTrewick/GCDSocket", from: "1.1.0")
     ],
     targets: [
        
